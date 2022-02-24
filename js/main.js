d4da74afee573e8d4d46ea55d2e539e0676834fb
serega-svarka <98324378+serega-svarka@users.noreply.github.com>
2022-02-24 10:32:33 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
 "use strict";
-
 jQuery(document).ready(function() {
 
     jQuery('#slider').slick();
 
 });
+
+
+var modal = document.getElementById("myModal");
+
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
